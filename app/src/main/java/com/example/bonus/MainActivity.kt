f333794eb7a01e7ae91bb5a33f1c72197933dc4f
ANANTIA TEMGOUA <elvado.temgoua@gmail.com>
2026-02-10 16:07:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bonus/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.bonus
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val email = findViewById<EditText>(R.id.email)
+//        val phone = findViewById<EditText>(R.id.phone)
+        val text = findViewById<TextView>(R.id.text)
+        val password = findViewById<EditText>(R.id.password)
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        text.text = "Welcome"
+
+        loginBtn.isEnabled = false
+
+        email.addTextChangedListener{
+            password.addTextChangedListener {
+                if (email.text.isNotEmpty()  && password.text.isNotEmpty()) {
+                    loginBtn.isEnabled = true
+                }
+            }
+        }
+
+        loginBtn.setOnClickListener{
+           if(email.text.isEmpty()){
+               text.text = "Enter Email"
+           }else if(password.text.toString() == ""){
+               text.text = "Enter password"
+           }else{
+               text.text = "Login successful"
+           }
+
+           val intent = Intent(this, LandingActivity::class.java)
+           startActivity((intent))
+       }
+    }
+}
---
+++ app/src/main/java/com/example/bonus/SignUpActivity.kt
@@ -0,0 +1,30 @@
+package com.example.bonus
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUpActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+
+        val signupBtn = findViewById<Button>(R.id.signupBtn)
+
+        signupBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity((intent))
+        }
+
+
+
+    }
+}
