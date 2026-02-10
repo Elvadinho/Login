78e441fa2617b85d32131f8b6731125522bbd9c4
ANANTIA TEMGOUA <elvado.temgoua@gmail.com>
2026-02-10 19:40:34 +0100

    revert back the checks on login

--- app/src/main/java/com/example/bonus/MainActivity.kt
+++ app/src/main/java/com/example/bonus/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.util.Patterns
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,45 +15,60 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var loginBtn: Button
+    private lateinit var text: TextView
+
     @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = findViewById<EditText>(R.id.email)
-//        val phone = findViewById<EditText>(R.id.phone)
-        val text = findViewById<TextView>(R.id.text)
-        val password = findViewById<EditText>(R.id.password)
-        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        email = findViewById(R.id.email)
+        text = findViewById(R.id.text)
+        password = findViewById(R.id.password)
+        loginBtn = findViewById(R.id.loginBtn)
+
+
         val signBtn = findViewById<Button>(R.id.signBtn)
         text.text = "Welcome"
 
         loginBtn.isEnabled = false
 
-        email.addTextChangedListener{
-            password.addTextChangedListener {
-                if (email.text.isNotEmpty()  && password.text.isNotEmpty()) {
-                    loginBtn.isEnabled = true
-                }
-            }
+        email.addTextChangedListener {
+            validateInput()
         }
 
-        loginBtn.setOnClickListener{
-           if(email.text.isEmpty()){
-               text.text = "Enter Email"
-           }else if(password.text.toString() == ""){
-               text.text = "Enter password"
-           }else{
-               text.text = "Login successful"
-           }
+        password.addTextChangedListener {
+            validateInput()
+        }
 
-           val intent = Intent(this, LandingActivity::class.java)
-           startActivity((intent))
-       }
+        loginBtn.setOnClickListener {
+            if (email.text.isEmpty()) {
+                text.text = "Enter Email"
+            } else if (password.text.toString() == "") {
+                text.text = "Enter password"
+            } else {
+                text.text = "Login successful"
+            }
+
+            val intent = Intent(this, LandingActivity::class.java)
+            startActivity((intent))
+        }
 
         signBtn.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity((intent))
         }
+    }
+    private fun validateInput() {
+        val emailText = email.text.toString()
+        val passwordText = password.text.toString()
+
+        val isEmailValid = Patterns.EMAIL_ADDRESS.matcher(emailText).matches()
+        val isPasswordValid = passwordText.length >= 8
+
+        loginBtn.isEnabled = isEmailValid && isPasswordValid
     }
 }
