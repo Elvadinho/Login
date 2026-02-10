fd007a2bd9228a1bdd9eb9da68c640bb0ea7508a
ANANTIA TEMGOUA <elvado.temgoua@gmail.com>
2026-02-10 19:58:55 +0100

    signup

--- app/src/main/java/com/example/bonus/SignUpActivity.kt
+++ app/src/main/java/com/example/bonus/SignUpActivity.kt
@@ -3,13 +3,22 @@ package com.example.bonus
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var genderRadioGroup: RadioGroup
+    private lateinit var signupBtn: Button
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +26,32 @@ class SignUpActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
 
-        val signupBtn = findViewById<Button>(R.id.signupBtn)
+        emailEditText = findViewById(R.id.email)
+        passwordEditText = findViewById(R.id.password)
+        genderRadioGroup = findViewById(R.id.gender_radio_group)
+        signupBtn = findViewById(R.id.signupBtn)
+
+        signupBtn.isEnabled = false
+
+        emailEditText.addTextChangedListener { validateFields() }
+        passwordEditText.addTextChangedListener { validateFields() }
+        genderRadioGroup.setOnCheckedChangeListener { _, _ -> validateFields() }
+
 
         signupBtn.setOnClickListener {
             val intent = Intent(this, LandingActivity::class.java)
             startActivity((intent))
         }
+    }
 
+    private fun validateFields() {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+        val genderSelected = genderRadioGroup.checkedRadioButtonId != -1
 
+        val isEmailValid = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        val isPasswordValid = password.length >= 8
 
+        signupBtn.isEnabled = isEmailValid && isPasswordValid && genderSelected
     }
 }
