5283adaaae2f9c3657963781fad193fdc0a7dbd8
ANANTIA TEMGOUA <elvado.temgoua@gmail.com>
2026-02-11 20:29:27 +0100

    Added a logout option

--- app/src/main/java/com/example/bonus/LandingActivity.kt
+++ app/src/main/java/com/example/bonus/LandingActivity.kt
@@ -1,13 +1,15 @@
 package com.example.bonus
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LandingActivity : AppCompatActivity() {
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
@@ -15,6 +17,7 @@ class LandingActivity : AppCompatActivity() {
         val emailTextView = findViewById<TextView>(R.id.email_text_view)
         val phoneTextView = findViewById<TextView>(R.id.phone_text_view)
         val genderTextView = findViewById<TextView>(R.id.gender_text_view)
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
 
         val email = intent.getStringExtra("email")
         val phone = intent.getStringExtra("phone")
@@ -23,5 +26,11 @@ class LandingActivity : AppCompatActivity() {
         emailTextView.text = "Email: $email"
         phoneTextView.text = "Phone: $phone"
         genderTextView.text = "Gender: $gender"
+
+        logoutBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
